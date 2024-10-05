cd42f661433916182d3fb008966ae1a2c05f06b1
SesifredoDev <93771023+SesifredoDev@users.noreply.github.com>
2024-10-05 14:37:53 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eco-points-frontend',
+  webDir: 'www'
+};
+
+export default config;
